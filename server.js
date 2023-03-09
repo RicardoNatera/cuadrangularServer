9200aa45932015185802a8c9f74a867b169434df
Ricardo Natera <ricardoantonionaterasubero@gmail.com>
2023-03-09 17:56:38 -0500

    allow origin

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.use('/general', generalRouter);
 
