e600834afce0213d6fd9a8507acd55b37e302061
Ricardo Natera <ricardoantonionaterasubero@gmail.com>
2023-03-10 10:11:57 -0500

    middleware

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", ["*"]);
   next();
 });
 
