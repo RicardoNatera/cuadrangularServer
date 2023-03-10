ed0ec8c9435ed121e8ed7052d5abe8148ef9b9c9
Ricardo Natera <ricardoantonionaterasubero@gmail.com>
2023-03-10 12:31:44 -0500

    todos

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", 'https://eloquent-beijinho-3c2d53.netlify.app/');
+  res.header("Access-Control-Allow-Origin", '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
